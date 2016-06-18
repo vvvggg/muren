a05c0765a1c18aa931e784f485eecc6741cf5e88
Vladimir Gusev <vladimir@gusev.pro>
2016-06-19 02:32:33 +0300

    initial blank

---
+++ muren.cpp
@@ -0,0 +1,21 @@
+//
+// Created by user on 19/06/16.
+//
+
+#include <iostream>
+#include "muren.h"
+
+using namespace std;
+
+int main( int argc, char * argv[] ) {
+
+    // get work root dir
+
+    // work
+    // afterwards
+    return 0;
+
+}
+
+
+
---
+++ muren.h
@@ -0,0 +1,11 @@
+//
+// Created by user on 19/06/16.
+//
+
+#ifndef MUREN_MUREN_H
+#define MUREN_MUREN_H
+
+#endif //MUREN_MUREN_H
+
+
+
